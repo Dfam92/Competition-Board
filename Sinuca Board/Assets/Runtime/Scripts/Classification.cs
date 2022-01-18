b12904841af1ddc9521ecfa225fc260f0af0afdd
Dfam92 <dfam92@hotmail.com>
2022-01-18 12:31:46 -0300

    Finish Classification and Start NextPhase Cycle
    Creating filters in dictionary for order the players according their status.
    Creating List Of TextMesh for Orderer the players in Classification Panel.
    SetNewPhase for reset all stuf and start the process again.

--- Sinuca Board/Assets/Runtime/Scripts/Classification.cs
+++ Sinuca Board/Assets/Runtime/Scripts/Classification.cs
@@ -11,22 +11,23 @@ public class Classification : MonoBehaviour
     [SerializeField] GameManager gameManager;
     [SerializeField] Text inputTextClassified;
     [SerializeField] GameObject classificationButton;
-    [SerializeField] GameObject setClassifiedsButton;
-    [SerializeField] GameObject InputField;
-    
+    [SerializeField] GameObject listOfPlayers;
+
+    [SerializeField] List<TextMeshProUGUI> leaderBoardText;
     public List<Player> classifiedPlayers;
-    public Dictionary<Player, int> leaderBoard = new Dictionary<Player,int>();
+    public Dictionary<Player, int> leaderBoard = new Dictionary<Player, int>();
+    public Dictionary<Player, int> classificationByBalls = new Dictionary<Player, int>();
 
     private int numberOfClassified;
     // Start is called before the first frame update
     void Start()
     {
-        if(gameManager.playerCompletedGames.Count == gameManager.numberOfPlayers)
+        if (gameManager.playerCompletedGames.Count == gameManager.numberOfPlayers)
         {
             for (int i = 0; i < gameManager.numberOfPlayers; i++)
             {
-                leaderBoard.Add(gameManager.players[i], gameManager.players[i].wins);
-                
+                classificationByBalls.Add(gameManager.players[i], gameManager.players[i].balls);
+
             }
         }
     }
@@ -34,32 +35,74 @@ public class Classification : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void StartClassification()
     {
         //Ordena o dicionario conforme o Key ou Value(atenção ao final da sentença.);
+        foreach (KeyValuePair<Player, int> item in classificationByBalls.OrderByDescending(ballsValue => ballsValue.Value))
+        {
+            classifiedPlayers.Add(item.Key);
+            Debug.Log($"{item.Key} + {item.Value}");
+        }
+        leaderBoard.Clear();
+        for (int i = 0; i < gameManager.numberOfPlayers; i++)
+        {
+            leaderBoard.Add(classifiedPlayers[i], classifiedPlayers[i].wins);
+        }
+        classifiedPlayers.Clear();
         foreach (KeyValuePair<Player, int> item in leaderBoard.OrderByDescending(winValue => winValue.Value))
         {
             classifiedPlayers.Add(item.Key);
+            Debug.Log($"{item.Key} + {item.Value}");
         }
+        for (int i = 0; i < gameManager.numberOfPlayers; i++)
+        {
+            leaderBoardText[i].text = classifiedPlayers[i].savedName.text;
+
+        }
+
         classificationButton.SetActive(false);
-        setClassifiedsButton.SetActive(true);
-        InputField.SetActive(true);
+        listOfPlayers.SetActive(true);
     }
     public void SetPlayers()
     {
         int.TryParse(inputTextClassified.text, out numberOfClassified);
         classifiedPlayers.RemoveRange(numberOfClassified, classifiedPlayers.Count - numberOfClassified);
+        leaderBoardText.RemoveRange(0, numberOfClassified);
+        for (int i = 0; i < leaderBoardText.Count; i++)
+        {
+            leaderBoardText[i].color = Color.red;
+        }
 
         for (int i = 0; i < numberOfClassified; i++)
         {
+
             classifiedPlayers[i].gameObjectPosition.SetActive(true);
         }
+
+        StartCoroutine(StartNewFase());
+
+    }
+
+    IEnumerator StartNewFase()
+    {
+        yield return new WaitForSeconds(5);
+        for (int i = 0; i < numberOfClassified; i++)
+        {
+            classifiedPlayers[i].balls = 0;
+            classifiedPlayers[i].wins = 0;
+            classifiedPlayers[i].myGames.Clear();
+            classifiedPlayers[i].victories.text = "-";
+            classifiedPlayers[i].playedGames.text = "-";
+            classifiedPlayers[i].ballInRole.text = "-";
+            classifiedPlayers[i].isCompleted = false;
+
+        }
+        gameManager.playerCompletedGames.Clear();
         gameManager.boxGenerator.SetActive(true);
         gameManager.battleMenu.SetActive(false);
         gameManager.classificationMenu.SetActive(false);
-        
     }
 }
